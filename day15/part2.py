4e9237e090257458d251b12e974c8ab4043915aa
Michael Noronha <michaeltnoronha@gmail.com>
2019-01-02 13:08:43 -0500

    Day 15

---
+++ day15/part2.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import re
+
+ingredients = []
+with open("input.txt") as f:
+    for line in f:
+        attrs = capacity, durability, flavor, texture, calories = list(map(int, re.findall("-?[0-9]", line)))
+        name = line.split()[0][:-1].lower()
+        ingredients.append(attrs)
+
+max_score = -1
+for sugar in range(0, 101):
+    for sprinkles in range(0, 101 - sugar):
+        for candy in range(0, 101 - sugar - sprinkles):
+            chocolate = 100 - sugar - sprinkles - candy
+
+            nums = [sugar, sprinkles, candy, chocolate]
+            capacity = max(0, sum([x * y for x, y in zip([i[0] for i in ingredients], nums)]))
+            durability = max(0, sum([x * y for x, y in zip([i[1] for i in ingredients], nums)]))
+            flavor = max(0, sum([x * y for x, y in zip([i[2] for i in ingredients], nums)]))
+            texture = max(0, sum([x * y for x, y in zip([i[3] for i in ingredients], nums)]))
+            calories = sum([x * y for x, y in zip([i[4] for i in ingredients], nums)])
+
+            if calories == 500:
+                score = capacity * durability * flavor * texture
+                max_score = max(max_score, score)
+
+print(max_score)
+
