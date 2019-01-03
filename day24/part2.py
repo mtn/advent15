269876a314559a554ba9bdfe7f0d9eee49c716f1
Michael Noronha <michaeltnoronha@gmail.com>
2019-01-02 22:01:57 -0500

    Day 24

---
+++ day24/part2.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+from itertools import combinations
+
+nums = []
+with open("input.txt") as f:
+    for line in f:
+        nums.append(int(line.strip()))
+
+per_group = sum(nums) // 4
+
+# try to find the smallest subset summing to this; see if it's correct
+# bad in general
+
+for n in range(len(nums)):
+    sub = [l for l in list(combinations(nums, n)) if sum(l) == per_group]
+    if len(sub) > 0:
+        break
+
+ans = 1
+for i in sub[0]:
+    ans *= i
+print(ans) # well, it worked (again)
